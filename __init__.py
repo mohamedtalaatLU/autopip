08d44cf82d528d1c2266e56c4deb0b72ebf1fe0e
mohamedtalaatLU <39444269+mohamedtalaatLU@users.noreply.github.com>
2018-08-26 04:59:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
-
+from pip import main as installer
+if not hasattr(installer, "main"):
+  del pip
+  from pip._internal import main as installer
+  
+if __name__ != "__main__":
+  try:
+    except ModuleNotFoundError:
+      installer(["install", pass])
+    
